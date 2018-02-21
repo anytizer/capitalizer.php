bef1a6db678b6d5829c1e9ab122b1595bc979efc
server <server@example.com>
2018-02-21 13:52:00 -0700

    phpunit psr4 fix

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 
 namespace tests;
 
-require_once(dirname(__FILE__)."/../src/libraries/classes/common/class.capitalizer.inc.php");
+require_once(dirname(__FILE__)."/../src/libraries/classes/common/capitalizer.php");
