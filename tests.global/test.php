ba0ab04838f90b2131ccbcee041ef79edc755225
server <server@example.com>
2018-03-18 17:39:28 -0600

    tests

--- tests.global/test.php
+++ tests.global/test.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 
 use common\capitalizer;
 use PHPUnit\Framework\TestCase;
+
 $capitalizer = new capitalizer();
 $word = $capitalizer->capitalize("user_id");
 echo $word;
---
+++ tests.remote/readme.md
@@ -0,0 +1,3 @@
+# Installation
+
+	composer require anytizer/capitalizer.php:dev-master
